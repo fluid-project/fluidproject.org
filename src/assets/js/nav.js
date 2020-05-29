650e0f797317686eda6c44a2802e0f10e6e642ba
sachin10101998 <schopra@mt.iitr.ac.in>
2020-05-30 01:09:06 +0530

    Removed package lock json file and fixed Nav link default CSS

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -1,13 +1,12 @@
 var linkk=window.location.pathname;
 var btnContainer = document.getElementById("fluid-web-nav");
 console.log(linkk);
-
 var index=document.getElementById("indexnav");
 var aboutnav=document.getElementById("aboutnav");
 var projectsnav=document.getElementById("projectsnav");
 var infusionnav=document.getElementById("infusionnav");
 var newsnav=document.getElementById("newsnav");
-if(linkk==""|| linkk=="/index.html")
+if(linkk=="/"|| linkk=="/index.html")
 {
   index.setAttribute("class", "button current");
 }
