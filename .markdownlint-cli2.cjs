ce0c4674970c72d5c63f8c851163f09a57a147cf
Ned Zimmerman <ned@bight.dev>
2023-09-11 09:18:03 -0300

    fix: ignore CHANGELOG in Markdown linting

--- .markdownlint-cli2.cjs
+++ .markdownlint-cli2.cjs
@@ -2,5 +2,5 @@
 
 module.exports = {
     config: require("./node_modules/markdownlint-config-fluid/.markdownlintrc.json"),
-    ignores: ["node_modules"]
+    ignores: ["node_modules", "CHANGELOG.md"]
 };
